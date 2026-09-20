81099d1e61b838346515d64297a64c6a7e47ebc3
Callum <Callumr714@icloud.com>
2026-09-20 15:16:44 +0100

    cuda example added

--- linearRegression.py
+++ linearRegression.py
@@ -14,20 +14,21 @@ class linearRegressionModel(nn.Module):
         return self.linear_layer(x)
 
 
-N = 10
-
+N = 10000
+print(torch.cuda.is_available())
 D_in = 1
 D_out = 1
 
-X = torch.randn(N, D_in)
-model = linearRegressionModel(D_in, D_out)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+X = torch.randn(N, D_in, device=device)
+model = linearRegressionModel(D_in, D_out).to(device)
 optimiser = optim.Adam(model.parameters(), lr=learning_rate)
 
 loss_fn = nn.MSELoss()
-tW = torch.tensor([[2.0]])
-tb = torch.tensor(1.0)
-y_true = X @ tW + tb + torch.randn(N, D_out) * 0.1
-epoch = 1000
+tW = torch.tensor([[2.0]], device=device)
+tb = torch.tensor(1.0, device=device)
+y_true = X @ tW + tb + torch.randn(N, D_out, device=device) * 0.1
+epoch = 1000000
 for i in range(epoch):
     y_hat = model(X)
     loss = loss_fn(y_hat, y_true)
@@ -36,5 +37,5 @@ for i in range(epoch):
     loss.backward()
     optimiser.step()
 
-    if i % 10 == 0:
+    if i % 1000 == 0:
         print(f"epoch {i}: loss = {loss.item():.4f}")
